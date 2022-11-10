6285b017a5b56d58052578cb1285577d79f6c4d8
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-11-10 19:43:06 +0000

    vbc

--- js/game.js
+++ js/game.js
@@ -3,8 +3,8 @@
 const config = {
   type: Phaser.AUTO, 
   width: 1920,
-  height 1080,
-  backgroundColor: 0x5f6e7a
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
 }
 
 const game = new Phaser.Game(config)
