57b2b7b6d2abf63515d8fe6eb980da301155e1b7
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-11-11 14:23:06 -0500

    tyuy67uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- js/game.js
+++ js/game.js
@@ -1,5 +1,9 @@
 /* global phaser */
-
+// Copyright (c) 2020 Ali Mugamai All rights reserved
+//
+// Created by: Ali Mugamai
+// Created on: NOV 2022
+// This file contains the JS functions for index.html
 const config = {
   type: Phaser.AUTO,
   width: 1920,
