f284e3be3f9aba5f2b376de939e412dc8d195603
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-11-21 14:14:31 -0500

    bug fixes

--- js/game.js
+++ js/game.js
@@ -4,11 +4,12 @@
 // Created by: Ali Mugamai
 // Created on: NOV 2022
 // This file contains the JS functions for index.html
-import SplashScene from "./splashScene.js";
-import TitleScene from "./titleScene.js";
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
 
-const splashScene = new SplashScene();
-const titleScene = new TitleScene();
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -27,8 +28,9 @@ const config = {
   },
 };
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
 
-game.scene.add("splashScene", splashScene);
-game.scene.add("titleScene", titleScene);
-game.scene.start("splashScene");
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+
+game.scene.start("splashScene")
