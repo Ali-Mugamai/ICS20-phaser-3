c84fd3b2e64d8c61c261b0a8a95a881178bd3679
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-11-25 02:29:47 +0000

    Prettified Code!

--- js/MenuScene.js
+++ js/MenuScene.js
@@ -6,26 +6,25 @@
 // This file contains the JS functions for index.html
 
 class MenuScene extends Phaser.Scene {
-    constructor() {
-      super({ key: "menuScene" });
-    }
-  
-    Infinity(data) {
-      this.cameras.main.setBackgroundColor("ffffff");
-    }
-  
-    preload() {
-      console.log("Menu Scene");
-    }
-  
-    create(data) {
-      //pass
-    }
-  
-    update(time, delta) {
-      //pass
-    }
+  constructor() {
+    super({ key: "menuScene" });
   }
-  
-  export default MenuScene;
-  
+
+  Infinity(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
+  }
+
+  preload() {
+    console.log("Menu Scene");
+  }
+
+  create(data) {
+    //pass
+  }
+
+  update(time, delta) {
+    //pass
+  }
+}
+
+export default MenuScene;
