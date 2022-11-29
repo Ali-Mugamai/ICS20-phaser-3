ca93fbacf7316c1c83fc687a3408ea94e9e9dc71
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-11-29 19:43:16 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -4,15 +4,15 @@
 // Created by: Ali Mugamai
 // Created on: NOV 2022
 // This file contains the JS functions for index.html
-import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
-import GameScene from "./gameScene.js"
+import SplashScene from "./splashScene.js";
+import TitleScene from "./titleScene.js";
+import MenuScene from "./menuScene.js";
+import GameScene from "./gameScene.js";
 
-const splashScene = new SplashScene()
-const titleScene = new TitleScene()
-const menuScene = new MenuScene()
-const gameScene = new GameScene()
+const splashScene = new SplashScene();
+const titleScene = new TitleScene();
+const menuScene = new MenuScene();
+const gameScene = new GameScene();
 
 const config = {
   type: Phaser.AUTO,
@@ -32,11 +32,11 @@ const config = {
   },
 };
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
 
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
-game.scene.add("gameScene", gameScene)
+game.scene.add("splashScene", splashScene);
+game.scene.add("titleScene", titleScene);
+game.scene.add("menuScene", menuScene);
+game.scene.add("gameScene", gameScene);
 
-game.scene.start("splashScene")
+game.scene.start("splashScene");
