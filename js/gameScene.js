9c8904dc1bbf505a82e432fb5e02a2a016e39e6f
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-12-08 13:41:09 -0500

    gtd

--- js/gameScene.js
+++ js/gameScene.js
@@ -38,7 +38,7 @@ class GameScene extends Phaser.Scene {
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT");
     const keyRightObj = this.input.keyboard.addKey("RIGHT");
-    const keySpaceObj = this.input.keyboard.addkey("SPACE");
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15;
