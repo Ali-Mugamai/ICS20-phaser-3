4c3db3698144d15f9b68af93065b09a4468aca7c
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-12-08 18:37:41 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null;
     this.ship = null;
-    this.fireMissle = false
+    this.fireMissle = false;
   }
 
   init(data) {
@@ -23,7 +23,7 @@ class GameScene extends Phaser.Scene {
 
     this.load.image("starBackground", "./assets/starBackground.png");
     this.load.image("ship", "./assets/spaceShip.png");
-    this.load.image("missile", "./assets/missile.png")
+    this.load.image("missile", "./assets/missile.png");
   }
 
   create(data) {
@@ -32,13 +32,13 @@ class GameScene extends Phaser.Scene {
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
-    this.missleGroup = this.physics.add.group()
+    this.missleGroup = this.physics.add.group();
   }
 
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT");
     const keyRightObj = this.input.keyboard.addKey("RIGHT");
-    const keySpaceObj = this.input.keyboard.addkey("SPACE")
+    const keySpaceObj = this.input.keyboard.addkey("SPACE");
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15;
@@ -48,21 +48,23 @@ class GameScene extends Phaser.Scene {
     }
     if (keyRightObj.isDown === true) {
       this.ship.x += 15;
-      if (this.ship.x > 1920) 
-        this.ship.x = 1920;
-      }
-    
+      if (this.ship.x > 1920) this.ship.x = 1920;
+    }
 
     if (keySpaceObj.isDown === true) {
       if (this.firemissile === false) {
-        this.firemissile = true
-        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
-        this.missileGroup.add(aNewMissile)
+        this.firemissile = true;
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "missile"
+        );
+        this.missileGroup.add(aNewMissile);
+      }
     }
-  }
 
-  if (keySpaceObj.isUp === true) {
-    this.firemissile = false
+    if (keySpaceObj.isUp === true) {
+      this.firemissile = false;
     }
   }
 }
