06db168146c706cb69b2d40e0351b4d084d21e8e
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-12-23 02:12:47 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -21,9 +21,13 @@ class GameScene extends Phaser.Scene {
     this.background = null;
     this.ship = null;
     this.fireMissle = false;
-    this.score = 0
-    this.scoreText = null
-    this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: 'center'}
+    this.score = 0;
+    this.scoreText = null;
+    this.scoreTextStyle = {
+      font: "65px Arial",
+      fill: "#ffffff",
+      align: "center",
+    };
   }
 
   init(data) {
@@ -46,7 +50,12 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
     this.background.setOrigin(0, 0);
 
-    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(
+      10,
+      10,
+      "Score: " + this.score.toString(),
+      this.scoreTextStyle
+    );
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
