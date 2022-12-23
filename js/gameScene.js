dd831275f0ae1a5fdfadb2821fdd090c218d29cf
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-12-23 00:12:17 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -25,7 +25,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("ship", "./assets/spaceShip.png");
     this.load.image("missile", "./assets/missile.png");
 
-    this.load.audio("laser", "./assets/laser1.wav")
+    this.load.audio("laser", "./assets/laser1.wav");
   }
 
   create(data) {
